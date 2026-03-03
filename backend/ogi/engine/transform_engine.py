5777facbc91e7d40ca83ab0cbfbef6ba996a547e
Yusuf Khasbulatov <khashashin@gmail.com>
2026-03-03 23:09:14 +0100

    feat: add URL to Outbound Links transform for extracting links from web pages

--- backend/ogi/engine/transform_engine.py
+++ backend/ogi/engine/transform_engine.py
@@ -108,6 +108,7 @@ class TransformEngine:
         from ogi.transforms.ip.ip_to_geolocation import IPToGeolocation
         from ogi.transforms.ip.ip_to_asn import IPToASN
         from ogi.transforms.web.url_to_headers import URLToHeaders
+        from ogi.transforms.web.url_to_links import URLToLinks
         from ogi.transforms.web.url_to_content import URLToContent
         from ogi.transforms.web.content_to_iocs import ContentToIOCs
         from ogi.transforms.web.domain_to_urls import DomainToURLs
@@ -122,7 +123,7 @@ class TransformEngine:
         for cls in [
             DomainToIP, DomainToMX, DomainToNS, IPToDomain, WhoisLookup,
             IPToGeolocation, IPToASN,
-            URLToHeaders, URLToContent, ContentToIOCs, DomainToURLs,
+            URLToHeaders, URLToLinks, URLToContent, ContentToIOCs, DomainToURLs,
             EmailToDomain, DomainToEmails,
             DomainToCerts, CertTransparency,
             UsernameSearch, HashLookup,
